2ea94b3f90db0b287edc89b6c0b6056c302f5e87
Rugal Tu <john2653589@gmail.com>
2025-06-03 18:21:09 +0800

    Rugal Update

--- Dtvl.Pvs/wwwroot/scripts/Sample/Input.js
+++ Dtvl.Pvs/wwwroot/scripts/Sample/Input.js
@@ -2,26 +2,20 @@ import { DtvlPv, Formats } from 'dtvlpv';
 import { Model } from '@rugal.tu/vuemodel3';
 Model.UpdateStore('Root.NumberInput', 123);
 Model.AddV_Tree('Root', {
-    ':NumberInput': {
-        'using': Paths => {
-            DtvlPv.AddPv_Input(Paths, {
-                Number: true,
-            });
-        },
+    ':NumberInput': Paths => {
+        DtvlPv.AddPv_Input(Paths, {
+            Number: true,
+        });
     },
-    ':AdDateInput': {
-        'using': Paths => {
-            DtvlPv.AddPv_Input(Paths, {
-                Format: Formats.AdDate,
-            });
-        },
+    ':AdDateInput': Paths => {
+        DtvlPv.AddPv_Input(Paths, {
+            Format: Formats.AdDate,
+        });
     },
-    ':TwDateInput': {
-        'using': Paths => {
-            DtvlPv.AddPv_Input(Paths, {
-                Format: Formats.TwDate,
-            });
-        },
+    ':TwDateInput': Paths => {
+        DtvlPv.AddPv_Input(Paths, {
+            Format: Formats.TwDate,
+        });
     }
 });
 //# sourceMappingURL=Input.js.map
